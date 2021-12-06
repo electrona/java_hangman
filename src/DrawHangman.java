f9288a53bac511af9e02361db129232424b3d149
neilstrattoncodes <95373851+neilstrattoncodes@users.noreply.github.com>
2021-12-05 21:51:40 -0330

    Update DrawHangman.java

--- src/DrawHangman.java
+++ src/DrawHangman.java
@@ -1,2 +1,182 @@
 public class DrawHangman {
+
+
+	public static void hangmandisplay() {
+
+		if (count == 1) {
+			System.out.println("Guess One!");
+			System.out.println("  __________..________      ");
+			System.out.println("  | |________))______|                 ");
+			System.out.println("  | |/ /                     ");
+			System.out.println("  | | /                                 ");
+			System.out.println("  | |/                               ");
+			System.out.println("  | |                             ");
+			System.out.println("  | |                                    ");
+			System.out.println("  | |                               ");
+			System.out.println("  | |                                   ");
+			System.out.println("  | |                                     ");
+			System.out.println("  | |                                     ");
+			System.out.println("  | |     `                    ");
+			System.out.println("  | |                                  ");
+			System.out.println("  | |                                  ");
+			System.out.println("  | |                                      ");
+			System.out.println("  | |____________________________                   ");
+			System.out.println("  | |____________________________|   ");
+			System.out.println("  | |____________________________|   ");
+			System.out.println("  | |____________________________|        ");
+			System.out.println("  | |___________________________ |        ");
+		}
+
+
+		if (count == 2) {
+			System.out.println("Guess Two!");
+			System.out.println("  __________..________      ");
+			System.out.println("  | |________))______|                 ");
+			System.out.println("  | |/ /     ||          ");
+			System.out.println("  | | /      ||                  ");
+			System.out.println("  | |/       || ,#####,                     ");
+			System.out.println("  | |        |/  X  X  |                     ");
+			System.out.println("  | |        ||    >    |             ");
+			System.out.println("  | |         \\,__o__,/            ");
+			System.out.println("  | |                           ");
+			System.out.println("  | |                                ");
+			System.out.println("  | |                                   ");
+			System.out.println("  | |                                    ");
+			System.out.println("  | |                                 ");
+			System.out.println("  | |                            ");
+			System.out.println("  | |                              ");
+			System.out.println("  | |____________________________                   ");
+			System.out.println("  | |____________________________|   ");
+			System.out.println("  | |____________________________|   ");
+			System.out.println("  | |____________________________|        ");
+			System.out.println("  | |___________________________ |        ");
+		}
+
+
+		if (count == 3) {
+			System.out.println("Guess Three!");
+			System.out.println("  __________..________      ");
+			System.out.println("  | |________))______|                 ");
+			System.out.println("  | |/ /     ||          ");
+			System.out.println("  | | /      ||                  ");
+			System.out.println("  | |/       || ,#####,                     ");
+			System.out.println("  | |        |/  X  X  |                     ");
+			System.out.println("  | |        ||    >    |             ");
+			System.out.println("  | |         \\,__o__,/            ");
+			System.out.println("  | |          --|  |--             ");
+			System.out.println("  | |          |      |             ");
+			System.out.println("  | |          |      |              ");
+			System.out.println("  | |          |______|           ");
+			System.out.println("  | |                              ");
+			System.out.println("  | |                                      ");
+			System.out.println("  | |                                   ");
+			System.out.println("  | |____________________________                   ");
+			System.out.println("  | |____________________________|   ");
+			System.out.println("  | |____________________________|   ");
+			System.out.println("  | |____________________________|        ");
+			System.out.println("  | |___________________________ |        ");
+		}
+
+		if (count == 4) {
+			System.out.println("Guess Four!");
+			System.out.println("  __________..________      ");
+			System.out.println("  | |________))______|                 ");
+			System.out.println("  | |/ /     ||          ");
+			System.out.println("  | | /      ||                  ");
+			System.out.println("  | |/       || ,#####,                     ");
+			System.out.println("  | |        |/  X  X  |                     ");
+			System.out.println("  | |        ||    >    |             ");
+			System.out.println("  | |         \\,__o__,/            ");
+			System.out.println("  | |      ||----|  |--            ");
+			System.out.println("  | |      ||--|      |                ");
+			System.out.println("  | |      ||  |      |                 ");
+			System.out.println("  | |     ` )  |______|                ");
+			System.out.println("  | |                                ");
+			System.out.println("  | |                                 ");
+			System.out.println("  | |                                  ");
+			System.out.println("  | |____________________________                   ");
+			System.out.println("  | |____________________________|   ");
+			System.out.println("  | |____________________________|   ");
+			System.out.println("  | |____________________________|        ");
+			System.out.println("  | |___________________________ |        ");
+		}
+
+
+		if (count == 5) {
+			System.out.println("Guess Five!");
+			System.out.println("  __________..________      ");
+			System.out.println("  | |________))______|                 ");
+			System.out.println("  | |/ /     ||          ");
+			System.out.println("  | | /      ||                  ");
+			System.out.println("  | |/       || ,#####,                     ");
+			System.out.println("  | |        |/  X  X  |                     ");
+			System.out.println("  | |        ||    >    |             ");
+			System.out.println("  | |         \\,__o__,/            ");
+			System.out.println("  | |      ||----|  |----||            ");
+			System.out.println("  | |      ||--|      |--||              ");
+			System.out.println("  | |      ||  |      |  ||             ");
+			System.out.println("  | |     ` )  |______|  ( '           ");
+			System.out.println("  | |                           ");
+			System.out.println("  | |                        ");
+			System.out.println("  | |                             ");
+			System.out.println("  | |____________________________                   ");
+			System.out.println("  | |____________________________|   ");
+			System.out.println("  | |____________________________|   ");
+			System.out.println("  | |____________________________|        ");
+			System.out.println("  | |___________________________ |        ");
+		}
+
+
+		if (count == 6) {
+			System.out.println("Guess Six, One Guess Left!");
+			System.out.println("  __________..________      ");
+			System.out.println("  | |________))______|                 ");
+			System.out.println("  | |/ /     ||          ");
+			System.out.println("  | | /      ||                  ");
+			System.out.println("  | |/       || ,#####,                     ");
+			System.out.println("  | |        |/  X  X  |                     ");
+			System.out.println("  | |        ||    >    |             ");
+			System.out.println("  | |         \\,__o__,/            ");
+			System.out.println("  | |      ||----|  |----||            ");
+			System.out.println("  | |      ||--|      |--||              ");
+			System.out.println("  | |      ||  |      |  ||             ");
+			System.out.println("  | |     ` )  |      |  ( '           ");
+			System.out.println("  | |          | |----|                 ");
+			System.out.println("  | |          | |                      ");
+			System.out.println("  | |          | |                        ");
+			System.out.println("  | |_________{  |_______________                   ");
+			System.out.println("  | |_________````  _____________|   ");
+			System.out.println("  | |____________________________|   ");
+			System.out.println("  | |____________________________|        ");
+			System.out.println("  | |___________________________ |        ");
+		}
+
+		if (count == 7) {
+			System.out.println("GAME OVER!");
+			System.out.println("  __________..________      ");
+			System.out.println("  | |________))______|                 ");
+			System.out.println("  | |/ /     ||          ");
+			System.out.println("  | | /      ||                  ");
+			System.out.println("  | |/       || ,#####,                     ");
+			System.out.println("  | |        |/  X  X  |                     ");
+			System.out.println("  | |        ||    >    |             ");
+			System.out.println("  | |         \\,__o__,/            ");
+			System.out.println("  | |      ||----|  |----||            ");
+			System.out.println("  | |      ||--|      |--||              ");
+			System.out.println("  | |      ||  |      |  ||             ");
+			System.out.println("  | |     ` )  |      |  ( '           ");
+			System.out.println("  | |          | |--| |                 ");
+			System.out.println("  | |          | |  | |                  ");
+			System.out.println("  | |          | |  | |                    ");
+			System.out.println("  | |_______  {  |  |  }   ______                   ");
+			System.out.println("  | |_______| ````  ````  /______|   ");
+			System.out.println("  | |_______|             /______|   ");
+			System.out.println("  | |________|__________/________|        ");
+			System.out.println("  | |___________________________ |        ");
+			System.out.println(" The word was plus ADD WORD HERE LATER");
+
+
+		}
+
+	}
 }
