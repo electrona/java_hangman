bccd1c4dd5576dfe7d5b7444c7f3582cdd4e659a
woolridge7 <95373639+woolridge7@users.noreply.github.com>
2021-12-06 17:54:56 -0330

    Help

--- src/DrawHangman.java
+++ src/DrawHangman.java
@@ -1,34 +1,41 @@
 public class DrawHangman {
 
-    public void drawHangman() {
-        for (int i = 0; i < 8; i++) {
-            if (i == 1) {
+    public void drawHangman(int lives) {
+        //int j = 7;
+        //for (int i = 0; i < 8; i++) {
+        if (lives == 7) {
                 System.out.println("lives left 6");
                 System.out.println(" O ");
-            } else if (i == 2) {
+            }
+        else if (lives == 6) {
                 System.out.println("lives left 5");
                 System.out.println(" O ");
                 System.out.println("/");
-            } else if (i == 3) {
+            }
+        else if (lives == 5) {
                 System.out.println("lives left 4");
                 System.out.println(" O ");
                 System.out.println("/| ");
-            } else if (i == 4) {
+            }
+        else if (lives == 4) {
                 System.out.println("lives left 3");
                 System.out.println(" O ");
                 System.out.println("/|\\");
-            } else if (i == 5) {
+            }
+        else if (lives == 3) {
                 System.out.println("lives left 2");
                 System.out.println(" O ");
                 System.out.println("/|\\");
                 System.out.println(" | ");
-            } else if (i == 6) {
+            }
+        else if (lives == 2) {
                 System.out.println("lives left 1");
                 System.out.println(" O ");
                 System.out.println("/|\\");
                 System.out.println(" | ");
                 System.out.println("/");
-            } else if (i == 7) {
+            }
+        else if (lives == 1) {
                 System.out.println("lives left 0");
                 System.out.println(" O ");
                 System.out.println("/|\\");
@@ -36,7 +43,9 @@ public class DrawHangman {
                 System.out.println("/\\");
                 System.out.println("you lose");
             }
+        //lives = lives - 1;
+        //return lives;
 
         }
     }
-    }
+
--- src/Dummy.java
+++ src/Dummy.java
@@ -1,32 +1,67 @@
 import java.util.Scanner;
-
-import java.util.Arrays;
-import java.util.Scanner;
 public class Dummy {
 
     public static void main(String[] args) {
-        String str = "cat";
+        DrawHangman draw = new DrawHangman();
+        String str = "banana";                           //change "word" to rnd word from WordBank
         char[] array = str.toCharArray();
         //System.out.print(array.length);
+        char[] remainingLetters = str.toCharArray();
 
         char[] completedWord = new char[array.length];
         for (int j = 0; j < completedWord.length; j++) {
             completedWord[j] = '_';
         }
+        System.out.println(completedWord);
+        int lives = 7;
+        int newCounter = 0;
+        int oldCounter = completedWord.length;
+        int counter = 0;
+       // int rightCounter = 0;
         Scanner kybd = new Scanner(System.in);
-        for (int k = 0; k < 5; k++) {                          //lives should be entered here
+        for (int k = 0; k < 100; k++) {                          //change to while loop
             System.out.println("Please enter your guess");
             char guess = kybd.next().charAt(0);
             //System.out.println(completedWord);
+            //int lives = 7;
             for (int i = 0; i <= (array.length - 1); i++) {
-                if (array[i] == guess)
+                if (array[i] == guess) {                         // checks if guess is correct
                     completedWord[i] = guess;
-            }
-            for(int h = 0; h < (completedWord.length); h++)
-                System.out.print(completedWord[h]);
-            System.out.println();
-            }
+                    //rightCounter = rightCounter + 1;
+                } else if (completedWord[i] == '_') {                     //identifying if guess was correct, if not use drawHangman()
+                    newCounter = newCounter + 1;
+                }
 
+                if (newCounter >= oldCounter) {
+                    draw.drawHangman(lives);
+                    System.out.println(oldCounter);
+                    System.out.println(newCounter);
+                    lives--;
+                    oldCounter = 0;
+                    newCounter = 0;
+                    if (lives == 0) {
+                        System.out.println(array);
+                        break;
+                    }
+                }
+            }
         }
+            for (int h = 0; h < (completedWord.length); h++)
+                System.out.print(completedWord[h]);
+
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] == completedWord[i]) {
+                    counter++;
+                    if(counter == array.length){
+                        break;
+                    }
+                }
+            }
+            System.out.println();//have to add a check for "_" to end loop
+    }
+        //lives--;
+
+
 }
+
 
