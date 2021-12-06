7c330fa951aa8ca8078ac259c1dac3639996567d
woolridge7 <95373639+woolridge7@users.noreply.github.com>
2021-12-06 18:30:03 -0330

    Win or Lose is now possible

--- src/Dummy.java
+++ src/Dummy.java
@@ -14,52 +14,48 @@ public class Dummy {
         }
         System.out.println(completedWord);
         int lives = 7;
-        int newCounter = 0;
-        int oldCounter = completedWord.length;
         int counter = 0;
-       // int rightCounter = 0;
+        int addedLetters = 0;
         Scanner kybd = new Scanner(System.in);
         for (int k = 0; k < 100; k++) {                          //change to while loop
             System.out.println("Please enter your guess");
             char guess = kybd.next().charAt(0);
             //System.out.println(completedWord);
-            //int lives = 7;
+            addedLetters = 0;
+            counter = 0;
             for (int i = 0; i <= (array.length - 1); i++) {
                 if (array[i] == guess) {                         // checks if guess is correct
                     completedWord[i] = guess;
-                    //rightCounter = rightCounter + 1;
-                } else if (completedWord[i] == '_') {                     //identifying if guess was correct, if not use drawHangman()
-                    newCounter = newCounter + 1;
-                }
-
-                if (newCounter >= oldCounter) {
-                    draw.drawHangman(lives);
-                    System.out.println(oldCounter);
-                    System.out.println(newCounter);
-                    lives--;
-                    oldCounter = 0;
-                    newCounter = 0;
-                    if (lives == 0) {
-                        System.out.println(array);
-                        break;
-                    }
+                    addedLetters = addedLetters + 1;
                 }
             }
-        }
+            if (addedLetters == 0) {
+                draw.drawHangman(lives);
+                lives--;
+                if (lives == 0) {
+                    System.out.println(array);
+                    break;
+                }
+            }
+
             for (int h = 0; h < (completedWord.length); h++)
                 System.out.print(completedWord[h]);
+            System.out.println();
 
-            for (int i = 0; i < array.length; i++) {
-                if (array[i] == completedWord[i]) {
+            for (int r = 0; r < array.length; r++) {            // checking if theres any blanks left
+                if (completedWord[r] != '_') {
                     counter++;
-                    if(counter == array.length){
-                        break;
-                    }
                 }
             }
-            System.out.println();//have to add a check for "_" to end loop
+            if (counter == array.length) {
+                System.out.println("You Win!!");
+                break;
+            }
+
+
+        }
+            //System.out.println();//have to add a check for "_" to end loop
     }
-        //lives--;
 
 
 }
