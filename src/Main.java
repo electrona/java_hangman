682e3a1c92fbfff481c28e7106dd8630ec0627f0
woolridge7 <95373639+woolridge7@users.noreply.github.com>
2021-12-06 20:35:23 -0330

    delete me

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.io.FileNotFoundException;
-import java.util.Arrays;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -29,22 +28,56 @@ public class Main {
         String word = wordBank.getRandomWord(wordList);
         word = word.toUpperCase();
 
-        char[] array = word.toCharArray();
-        //System.out.print(array.length);
+
+        //new
+        DrawHangman draw = new DrawHangman();
+        String str = word;                           //change "word" to rnd word from WordBank
+        char[] array = str.toCharArray();
 
         char[] completedWord = new char[array.length];
-        Arrays.fill(completedWord, '_');
+        for (int j = 0; j < completedWord.length; j++) {
+            completedWord[j] = '_';
+        }
+        System.out.println(completedWord);
+        int lives = 7;
+        int counter;
+        int addedLetters;
         Scanner kybd = new Scanner(System.in);
-        for (int k = 0; k < 5; k++) {                          //lives should be entered here
+        for (int k = 0; k < 100; k++) {                          //change to while loop
             System.out.println("Please enter your guess");
             char guess = kybd.next().charAt(0);
-            //System.out.println(completedWord);
+            addedLetters = 0;
+            counter = 0;
             for (int i = 0; i <= (array.length - 1); i++) {
-                if (array[i] == guess)
+                if (array[i] == guess) {                         // checks if guess is correct
                     completedWord[i] = guess;
+                    addedLetters = addedLetters + 1;
+                }
             }
-            for (char c : completedWord) System.out.print(c);
+            if (addedLetters == 0) {
+                draw.drawHangman(lives);
+                lives--;
+                if (lives == 0) {
+                    System.out.println(array);
+                    break;
+                }
+            }
+
+            for (int h = 0; h < (completedWord.length); h++)
+                System.out.print(completedWord[h]);
             System.out.println();
+
+            for (int r = 0; r < array.length; r++) {            // checking if theres any blanks left
+                if (completedWord[r] != '_') {
+                    counter++;
+                }
+            }
+            if (counter == array.length) {
+                System.out.println("You Win!!");
+                break;
+            }
         }
     }
 }
+
+
