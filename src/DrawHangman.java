137cb4853d6f0f34e3b374db87a02f2bb052b087
opethdeliverance <stephen.jwalsh@yahoo.com>
2021-12-08 15:06:56 -0330

    huh??

--- src/DrawHangman.java
+++ src/DrawHangman.java
@@ -1,41 +1,32 @@
 public class DrawHangman {
-
     public void drawHangman(int lives) {
         if (lives == 7) {
             System.out.println(" O ");
             System.out.println("lives left 6");
-            }
-        else if (lives == 6) {
+        } else if (lives == 6) {
             System.out.println(" O ");
             System.out.println("/");
             System.out.println("lives left 5");
-            }
-        else if (lives == 5) {
+        } else if (lives == 5) {
             System.out.println(" O ");
             System.out.println("/| ");
             System.out.println("lives left 4");
-            }
-        else if (lives == 4) {
+        } else if (lives == 4) {
             System.out.println(" O ");
             System.out.println("/|\\");
             System.out.println("lives left 3");
-            }
-        else if (lives == 3) {
-
+        } else if (lives == 3) {
             System.out.println(" O ");
             System.out.println("/|\\");
             System.out.println(" | ");
             System.out.println("lives left 2");
-            }
-        else if (lives == 2) {
-
+        } else if (lives == 2) {
             System.out.println(" O ");
             System.out.println("/|\\");
             System.out.println(" | ");
             System.out.println("/");
             System.out.println("lives left 1");
-            }
-        else if (lives == 1) {
+        } else if (lives == 1) {
             System.out.println(" O ");
             System.out.println("/|\\");
             System.out.println(" | ");
@@ -43,7 +34,6 @@ public class DrawHangman {
             System.out.println();
             System.out.println("lives left 0");
             System.out.println("you lose");
-            }
         }
     }
-
+}
