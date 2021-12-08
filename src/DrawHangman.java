e3c9cc6f8dacd58255b0be47b5ef46e1d7c9b441
Eric Thomas <ericthomas@tuta.io>
2021-12-08 16:29:39 -0330

    updated hangman drawing with Neil's drawings, fixed capitalization

--- src/DrawHangman.java
+++ src/DrawHangman.java
@@ -1,39 +1,167 @@
 public class DrawHangman {
+
     public void drawHangman(int lives) {
         if (lives == 7) {
-            System.out.println(" O ");
-            System.out.println("lives left 6");
-        } else if (lives == 6) {
-            System.out.println(" O ");
-            System.out.println("/");
-            System.out.println("lives left 5");
-        } else if (lives == 5) {
-            System.out.println(" O ");
-            System.out.println("/| ");
-            System.out.println("lives left 4");
-        } else if (lives == 4) {
-            System.out.println(" O ");
-            System.out.println("/|\\");
-            System.out.println("lives left 3");
-        } else if (lives == 3) {
-            System.out.println(" O ");
-            System.out.println("/|\\");
-            System.out.println(" | ");
-            System.out.println("lives left 2");
-        } else if (lives == 2) {
-            System.out.println(" O ");
-            System.out.println("/|\\");
-            System.out.println(" | ");
-            System.out.println("/");
-            System.out.println("lives left 1");
-        } else if (lives == 1) {
-            System.out.println(" O ");
-            System.out.println("/|\\");
-            System.out.println(" | ");
-            System.out.println("/ \\");
-            System.out.println();
-            System.out.println("lives left 0");
-            System.out.println("you lose");
+            System.out.println("  __________..________      ");
+            System.out.println("  | |________))______|                 ");
+            System.out.println("  | |/ /                     ");
+            System.out.println("  | | /                                 ");
+            System.out.println("  | |/                               ");
+            System.out.println("  | |                             ");
+            System.out.println("  | |                                    ");
+            System.out.println("  | |                               ");
+            System.out.println("  | |                                   ");
+            System.out.println("  | |                                     ");
+            System.out.println("  | |                                     ");
+            System.out.println("  | |     `                    ");
+            System.out.println("  | |                                  ");
+            System.out.println("  | |                                  ");
+            System.out.println("  | |                                      ");
+            System.out.println("  | |____________________________                   ");
+            System.out.println("  | |____________________________|   ");
+            System.out.println("  | |____________________________|   ");
+            System.out.println("  | |____________________________|        ");
+            System.out.println("  | |____________________________|        ");
+            System.out.println("Lives left: 6");
+            }
+        else if (lives == 6) {
+            System.out.println("  __________..________      ");
+            System.out.println("  | |________))______|                 ");
+            System.out.println("  | |/ /     ||          ");
+            System.out.println("  | | /      ||                  ");
+            System.out.println("  | |/       || ,#####,                     ");
+            System.out.println("  | |        |/  0  0  |                     ");
+            System.out.println("  | |        ||    >    |             ");
+            System.out.println("  | |         \\,__o__,/            ");
+            System.out.println("  | |                           ");
+            System.out.println("  | |                                ");
+            System.out.println("  | |                                   ");
+            System.out.println("  | |                                    ");
+            System.out.println("  | |                                 ");
+            System.out.println("  | |                            ");
+            System.out.println("  | |                              ");
+            System.out.println("  | |____________________________                   ");
+            System.out.println("  | |____________________________|   ");
+            System.out.println("  | |____________________________|   ");
+            System.out.println("  | |____________________________|        ");
+            System.out.println("  | |____________________________|        ");
+            System.out.println("Lives left: 5");
+            }
+        else if (lives == 5) {
+            System.out.println("  __________..________      ");
+            System.out.println("  | |________))______|                 ");
+            System.out.println("  | |/ /     ||          ");
+            System.out.println("  | | /      ||                  ");
+            System.out.println("  | |/       || ,#####,                     ");
+            System.out.println("  | |        |/  0  0  |                     ");
+            System.out.println("  | |        ||    >    |             ");
+            System.out.println("  | |         \\,__o__,/            ");
+            System.out.println("  | |          --|  |--             ");
+            System.out.println("  | |          |      |             ");
+            System.out.println("  | |          |      |              ");
+            System.out.println("  | |          |______|           ");
+            System.out.println("  | |                              ");
+            System.out.println("  | |                                      ");
+            System.out.println("  | |                                   ");
+            System.out.println("  | |____________________________                   ");
+            System.out.println("  | |____________________________|   ");
+            System.out.println("  | |____________________________|   ");
+            System.out.println("  | |____________________________|        ");
+            System.out.println("  | |____________________________|        ");
+            System.out.println("Lives left: 4");
+            }
+        else if (lives == 4) {
+            System.out.println("  __________..________      ");
+            System.out.println("  | |________))______|                 ");
+            System.out.println("  | |/ /     ||          ");
+            System.out.println("  | | /      ||                  ");
+            System.out.println("  | |/       || ,#####,                     ");
+            System.out.println("  | |        |/  0  0  |                     ");
+            System.out.println("  | |        ||    >    |             ");
+            System.out.println("  | |         \\,__o__,/            ");
+            System.out.println("  | |      ||----|  |--            ");
+            System.out.println("  | |      ||--|      |                ");
+            System.out.println("  | |      ||  |      |                 ");
+            System.out.println("  | |     ` )  |______|                ");
+            System.out.println("  | |                                ");
+            System.out.println("  | |                                 ");
+            System.out.println("  | |                                  ");
+            System.out.println("  | |____________________________                   ");
+            System.out.println("  | |____________________________|   ");
+            System.out.println("  | |____________________________|   ");
+            System.out.println("  | |____________________________|        ");
+            System.out.println("  | |____________________________|        ");
+            System.out.println("Lives left: 3");
+            }
+        else if (lives == 3) {
+            System.out.println("  __________..________      ");
+            System.out.println("  | |________))______|                 ");
+            System.out.println("  | |/ /     ||          ");
+            System.out.println("  | | /      ||                  ");
+            System.out.println("  | |/       || ,#####,                     ");
+            System.out.println("  | |        |/  0  0  |                     ");
+            System.out.println("  | |        ||    >    |             ");
+            System.out.println("  | |         \\,__o__,/            ");
+            System.out.println("  | |      ||----|  |----||            ");
+            System.out.println("  | |      ||--|      |--||              ");
+            System.out.println("  | |      ||  |      |  ||             ");
+            System.out.println("  | |     ` )  |______|  ( '           ");
+            System.out.println("  | |                           ");
+            System.out.println("  | |                        ");
+            System.out.println("  | |                             ");
+            System.out.println("  | |____________________________                   ");
+            System.out.println("  | |____________________________|   ");
+            System.out.println("  | |____________________________|   ");
+            System.out.println("  | |____________________________|        ");
+            System.out.println("  | |____________________________|        ");
+            System.out.println("Lives left: 2");
+            }
+        else if (lives == 2) {
+            System.out.println("  __________..________      ");
+            System.out.println("  | |________))______|                 ");
+            System.out.println("  | |/ /     ||          ");
+            System.out.println("  | | /      ||                  ");
+            System.out.println("  | |/       || ,#####,                     ");
+            System.out.println("  | |        |/  0  0  |                     ");
+            System.out.println("  | |        ||    >    |             ");
+            System.out.println("  | |         \\,__o__,/            ");
+            System.out.println("  | |      ||----|  |----||            ");
+            System.out.println("  | |      ||--|      |--||              ");
+            System.out.println("  | |      ||  |      |  ||             ");
+            System.out.println("  | |     ` )  |      |  ( '           ");
+            System.out.println("  | |          | |----|                 ");
+            System.out.println("  | |          | |                      ");
+            System.out.println("  | |          | |                        ");
+            System.out.println("  | |_________{  |_______________                   ");
+            System.out.println("  | |_________````_______________|   ");
+            System.out.println("  | |____________________________|   ");
+            System.out.println("  | |____________________________|        ");
+            System.out.println("  | |____________________________|        ");
+            System.out.println("Lives left: 1");
+            }
+        else if (lives == 1) {
+            System.out.println("  __________..________      ");
+            System.out.println("  | |________))______|                 ");
+            System.out.println("  | |/ /     ||          ");
+            System.out.println("  | | /      ||                  ");
+            System.out.println("  | |/       || ,#####,                     ");
+            System.out.println("  | |        |/  X  X  |                     ");
+            System.out.println("  | |        ||    >    |             ");
+            System.out.println("  | |         \\,__o__,/            ");
+            System.out.println("  | |      ||----|  |----||            ");
+            System.out.println("  | |      ||--|      |--||              ");
+            System.out.println("  | |      ||  |      |  ||             ");
+            System.out.println("  | |     ` )  |      |  ( '           ");
+            System.out.println("  | |          | |--| |                 ");
+            System.out.println("  | |          | |  | |                  ");
+            System.out.println("  | |          | |  | |                    ");
+            System.out.println("  | |_______  {  |  |  }   ______                   ");
+            System.out.println("  | |_______| ````  ````  /______|   ");
+            System.out.println("  | |_______|             /______|   ");
+            System.out.println("  | |________|__________/________|        ");
+            System.out.println("  | |____________________________|        ");
+            System.out.println("GAME OVER");
+            }
         }
     }
-}
+
