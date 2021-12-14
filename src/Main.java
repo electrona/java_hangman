13b32cac30ce88720fbf883f35e9cff8e9fb2e9a
woolridge7 <95373639+woolridge7@users.noreply.github.com>
2021-12-14 18:21:17 -0330

    tidied up

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,23 @@
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(System.in);
         WordBank wordBank = new WordBank();
         Game game = new Game();
+        String playAgain;
+        do {
         String wordList = wordBank.chooseList();
         String word = wordBank.getRandomWord(wordList);
         word = word.toLowerCase();
-        game.playGame(word);
+            game.playGame(word);
+            System.out.println();
+            System.out.print("Would you like to play again? (y/n) ");
+            playAgain = sc.nextLine();
+            System.out.println();
+        } while (playAgain.equals("y"));
 
 
     }
